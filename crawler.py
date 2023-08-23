e53d209a905a1eee05090cfe082928742b8d4f51
nhlong1512 <20521569@gm.uit.edu.vn>
2023-08-24 02:03:46 +0700

    push

--- crawler.py
+++ crawler.py
@@ -1,4 +1,5 @@
 from ctypes.wintypes import SERVICE_STATUS_HANDLE
+from telnetlib import EC
 import numpy as np
 from selenium import webdriver
 from time import sleep
@@ -6,141 +7,155 @@ import random
 import os
 from selenium.common.exceptions import NoSuchElementException, ElementNotInteractableException, ElementClickInterceptedException, StaleElementReferenceException
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
 import pandas as pd
 
 #Declare browsers
 url_file_driver = os.path.join('etc', 'chromedriver.exe')
 service = SERVICE_STATUS_HANDLE(executable_path=url_file_driver)
 driver = webdriver.Chrome(service=service)
-
-# Open URL
-driver.get("https://www.tripadvisor.com.vn/Hotels-g293921-Vietnam-Hotels.html")
-sleep(random.randint(10,15))
-try: 
-    see_all_btn = driver.find_element("xpath", "/html/body/div[1]/main/div[3]/div/div[2]/div/div[1]/div[2]/div[3]/div/div[12]/div/button")
-    see_all_btn.click()
-except NoSuchElementException:
-    try:
-        sleep(random.randint(10,15))
-        see_all_btn = driver.find_element("xpath", "/html/body/div[1]/main/div[3]/div/div[2]/div/div[1]/div[2]/div[3]/div/div[12]/div/button")
-        see_all_btn.click()
-    except NoSuchElementException:
-        print("No hope to click see all button")
-        pass
-    pass
-elems = driver.find_elements(By.CSS_SELECTOR, ".jsTLT [href]")
-links = [elem.get_attribute('href') for elem in elems]
-titles = [elem_title.text for elem_title in elems] 
-
 # Create lists to store data
 data_list = []
 
-
-for link in links:
-    title = -1
-    price = -1
-    rating = -1
-    address = -1
-    img_url = -1
-    description = -1
+def wait_and_click_see_all_btn():
+    max_attempts = 5  # Số lần thử tối đa
+    attempt = 0
     
-    driver.get(link)
-    elem = driver.find_element(By.CSS_SELECTOR, ".jvqAy")
-    title = driver.find_element(By.CSS_SELECTOR, ".jvqAy").text
-    rating = driver.find_element(By.CSS_SELECTOR, ".uwJeR").text
-    #Image url
-    img_url = driver.find_element("xpath", "/html/body/div[2]/div[2]/div[1]/div[1]/div[2]/div[3]/div/div/div/div/div[1]/div/div[1]/div/ul/li[1]/div/div/picture/source[1]").get_attribute("srcset")
-    
-    #Price
-    try: 
-        price = driver.find_element(By.CSS_SELECTOR, ".JPNOn").text
-    except NoSuchElementException:
+    while attempt < max_attempts:
         try:
-            price = driver.find_element(By.CSS_SELECTOR, ".WXMFC").text
-        except:
-            pass
-        pass
-    except StaleElementReferenceException: 
-        pass
-    
-    #Address
-    try: 
-        address = driver.find_element("xpath", "/html/body/div[2]/div[2]/div[2]/div[6]/div/div/div/div/div/div[4]/div[1]/div[2]/span[2]/span").text
-        print("Address --> ", address)
-    except:
+            see_all_btn = driver.find_element("xpath", "/html/body/div[1]/main/div[3]/div/div[2]/div/div[1]/div[2]/div[3]/div/div[12]/div/button")
+            see_all_btn.click()
+            return  # Thoát khỏi vòng lặp nếu nhấp thành công
+        except NoSuchElementException:
+            print("See All button not found, attempt:", attempt + 1)
+        except ElementNotInteractableException:
+            print("Failed to click See All button, attempt:", attempt + 1)
+            
+        attempt += 1
+        sleep(3)  # Đợi 3 giây trước khi thử lại
+
+
+def crawl_one_page(url_page): 
+    # Open URL
+    print('URL_PAGE_IN_FUNCTION: -----> ', url_page)
+    driver.get(url_page)
+    sleep(random.randint(10,15))
+    wait_and_click_see_all_btn()
+    elems = driver.find_elements(By.CSS_SELECTOR, ".jsTLT [href]")
+    links = [elem.get_attribute('href') for elem in elems]
+
+    for link in links:
+        title = -1
+        price = -1
+        rating = -1
+        address = -1
+        img_url = -1
+        description = -1
+        rank_of_province = -1
+        
+        driver.get(link)
+        title = driver.find_element(By.CSS_SELECTOR, ".jvqAy").text
+        rating = driver.find_element(By.CSS_SELECTOR, ".uwJeR").text
+        #Image url
+        img_url = driver.find_element("xpath", "/html/body/div[2]/div[2]/div[1]/div[1]/div[2]/div[3]/div/div/div/div/div[1]/div/div[1]/div/ul/li[1]/div/div/picture/source[1]").get_attribute("srcset")
+        
+        #Price
         try: 
-            address = driver.find_element(By.CSS_SELECTOR, ".fHvkI").text
-        except: 
-            pass
-        pass
-    
-    #Description
-    try: 
-        description = driver.find_element(By.CSS_SELECTOR, ".fIrGe").text
-    except: 
-        try: 
-            description = driver.find_element(By.CSS_SELECTOR, ".QewHA").text
-        except:
-            try: 
-                description = driver.find_element("xpath", "/html/body/div[2]/div[2]/div[2]/div[9]/div/div[1]/div[1]/div/div/div[3]/div[3]/div[2]/div[3]/div[1]/div[1]/span").text                                          
+            price = driver.find_element(By.CSS_SELECTOR, ".JPNOn").text
+        except NoSuchElementException:
+            try:
+                price = driver.find_element(By.CSS_SELECTOR, ".WXMFC").text
             except:
                 pass
-            pass    
-        pass
+            pass
+        except StaleElementReferenceException: 
+            pass
         
-    data_list.append({
-        "Title": title,
-        "Price": price,
-        "Rating": rating,
-        "Address": address,
-        "Img_URL": img_url,
-        "Description": description
-    })
+        #Address
+        try: 
+            address = driver.find_element("xpath", "/html/body/div[2]/div[2]/div[2]/div[6]/div/div/div/div/div/div[4]/div[1]/div[2]/span[2]/span").text
+        except NoSuchElementException:
+            try: 
+                address = driver.find_element("xpath", "/html/body/div[2]/div[1]/div/div[6]/div/div/div/div[2]/div/div[2]/div/div/div/span[2]/span").text
+            except: 
+                address = driver.find_element(By.CSS_SELECTOR, ".fHvkI").text
+                pass
+            pass
+        except StaleElementReferenceException:
+            try: 
+                address = driver.find_element("xpath", "/html/body/div[2]/div[1]/div/div[6]/div/div/div/div[2]/div/div[2]/div/div/div/span[2]/span").text
+            except: 
+                address = driver.find_element(By.CSS_SELECTOR, ".fHvkI").text
+                pass
+            pass
+        except ElementClickInterceptedException:
+            try: 
+                address = driver.find_element("xpath", "/html/body/div[2]/div[1]/div/div[6]/div/div/div/div[2]/div/div[2]/div/div/div/span[2]/span").text
+            except: 
+                address = driver.find_element(By.CSS_SELECTOR, ".fHvkI").text
+                pass
+            pass
+        
+        #Description
+        try: 
+            description = driver.find_element(By.CSS_SELECTOR, ".fIrGe").text
+        except NoSuchElementException: 
+            try: 
+                description = driver.find_element(By.CSS_SELECTOR, ".QewHA").text
+            except:
+                try: 
+                    description = driver.find_element("xpath", "/html/body/div[2]/div[2]/div[2]/div[9]/div/div[1]/div[1]/div/div/div[3]/div[3]/div[2]/div[3]/div[1]/div[1]/span").text                                          
+                except:
+                    pass
+                pass    
+            pass
+        except StaleElementReferenceException: 
+            try: 
+                description = driver.find_element(By.CSS_SELECTOR, ".QewHA").text
+            except:
+                try: 
+                    description = driver.find_element("xpath", "/html/body/div[2]/div[2]/div[2]/div[9]/div/div[1]/div[1]/div/div/div[3]/div[3]/div[2]/div[3]/div[1]/div[1]/span").text                                          
+                except:
+                    pass
+                pass    
+            pass
+        #Rank of province
+        try: 
+            rank_of_province = driver.find_element("xpath", "/html/body/div[2]/div[1]/div/div[6]/div/div/div/div[1]/div[2]/div/a/span").text
+        except NoSuchElementException:
+            pass
+            
+            
+        data_list.append({
+            "Title": title,
+            "Price": price,
+            "Rating": rating,
+            "Address": address,
+            "Img_URL": img_url,
+            "Description": description,
+            "Rank_of_province": rank_of_province,
+        }) 
+        sleep(random.randint(5,10))  
+        driver.close()
+
+    
+
+#Loop crawl pages
+for page_index in range(0,100): 
+    page_oa = page_index * 30
+    print('pageOA --> ', page_oa)
+    urlPage=''
+    if(page_index == 0): urlPage = 'https://www.tripadvisor.com.vn/Hotels-g293921-Vietnam-Hotels.html'
+    if(page_index != 0): urlPage = 'https://www.tripadvisor.com.vn/Hotels-g293921-oa' + str(page_oa) + '-Vietnam-Hotels.html'
+    print(urlPage)
+    crawl_one_page(urlPage)
+    
 
 # Create DataFrame from the list of dictionaries
 df = pd.DataFrame(data_list)
 
 # Print the DataFrame
 print(df)
-df.to_csv('./data/crawlTripAdvisor4.csv', encoding='utf-8', index=False)
+df.to_csv('./data/CrawlTripAdvisor_Hotel.csv', encoding='utf-16', index=False)
 
 driver.close()
-# driver.get(links[0])
-# elem = driver.find_elements(By.CSS_SELECTOR, ".jvqAy")
-# title = driver.find_element(By.CSS_SELECTOR, ".jvqAy").text
-# price = driver.find_element(By.CSS_SELECTOR, ".JPNOn").text
-# rating = driver.find_element(By.CSS_SELECTOR, ".uwJeR").text
-# address = driver.find_element(By.CSS_SELECTOR, ".fHvkI").text
-
-# img_url = driver.find_element("xpath", "/html/body/div[2]/div[2]/div[1]/div[1]/div[2]/div[3]/div/div/div/div/div[1]/div/div[1]/div/ul/li[1]/div/div/picture/source[1]").get_attribute("srcset")
-# print('Title --> ', title)
-# print('Price --> ', price)
-# print('Rating --> ', rating)
-# print('Address --> ', address)
-# print('Img_url --> ', img_url)
-# try: 
-#     description = driver.find_element("xpath", "/html/body/div[2]/div[2]/div[2]/div[4]/div/div[1]/div[4]/div/div/div/div/div[2]/div[1]/div[8]/div/div[1]/div/p[1]").text
-#     print("Description --> ", description)
-# except NoSuchElementException: 
-#     print("NoSuchElementException")
-
-
-# for i in range(1,5): 
-#     print("Crawl Page ", i+1, ": ")
-#     url_page = 'https://www.tripadvisor.com.vn/Hotels-g293921-oa' + str(i*30) + '-Vietnam-Hotels.html'
-#     print(url_page)
-#     driver.get(url_page)
-#     sleep(random.randint(10,15))
-#     try:
-#         print("Click see all")    
-#         see_all_btn = driver.find_element("xpath", "/html/body/div[1]/main/div[3]/div/div[2]/div/div[1]/div[2]/div[3]/div/div[12]/div/button")
-#         see_all_btn.click()
-#     except ElementClickInterceptedException:
-#         print("ElementClickInterceptedException") 
-#         pass
-#     elems = driver.find_elements(By.CSS_SELECTOR, ".jsTLT [href]")
-#     links = [elem.get_attribute('href') for elem in elems]
-#     titles = [elem_title.text for elem_title in elems] 
-#     print('Titles page --> ', titles)
-#     print('Links --> ', links)
-    
